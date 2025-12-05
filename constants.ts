bfff1a7c7669604a26303c58d8c7c00a3227a2df
Cado <lucacaroaxel@gmail.com>
2025-12-05 12:45:10 +0100

    config: aggiunta costanti globali e configurazione quartieri di Bari

---
+++ constants.ts
@@ -0,0 +1,30 @@
+// In a real scenario, these would be loaded from the .txt files.
+// We embed a subset of the provided data here for the prototype to function immediately.
+
+export const RAW_MURAT_DATA = {
+  "features": [
+    { "properties": { "amenity": "parking" }, "geometry": { "coordinates": [ [ [16.8749534, 41.1063161] ] ] } }, // Simplified geometry for seeding
+    { "properties": { "amenity": "parking" }, "geometry": { "coordinates": [ [ [16.8763222, 41.106168] ] ] } },
+    { "properties": { "amenity": "parking" }, "geometry": { "coordinates": [ [ [16.8736805, 41.1026361] ] ] } },
+    { "properties": { "amenity": "parking" }, "geometry": { "coordinates": [ [ [16.87262, 41.1169071] ] ] } },
+    { "properties": { "amenity": "parking" }, "geometry": { "coordinates": [ [ [16.8784868, 41.0932398] ] ] } }
+  ]
+};
+
+export const RAW_CARRASSI_DATA = {
+  "features": [
+    { "properties": { "amenity": "parking" }, "geometry": { "coordinates": [ [ [16.8704035, 41.1184115] ] ] } },
+    { "properties": { "amenity": "parking" }, "geometry": { "coordinates": [ [ [16.8660968, 41.1189897] ] ] } },
+    { "properties": { "amenity": "parking" }, "geometry": { "coordinates": [ [ [16.8700446, 41.1188918] ] ] } },
+    { "properties": { "amenity": "parking" }, "geometry": { "coordinates": [ [ [16.8704745, 41.1181149] ] ] } }
+  ]
+};
+
+export const RAW_POGGIOFRANCO_DATA = {
+  "features": [
+    { "properties": { "amenity": "parking" }, "geometry": { "coordinates": [ [ [16.8539146, 41.0960479] ] ] } },
+    { "properties": { "amenity": "parking" }, "geometry": { "coordinates": [ [ [16.8652972, 41.1063403] ] ] } },
+    { "properties": { "amenity": "parking" }, "geometry": { "coordinates": [ [ [16.8620805, 41.0958532] ] ] } },
+    { "properties": { "amenity": "parking" }, "geometry": { "coordinates": [ [ [16.8514744, 41.0934346] ] ] } }
+  ]
+};
